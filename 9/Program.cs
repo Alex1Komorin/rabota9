5eef77ec4a8ad28f538c18bc8af795035814895d
Shargin Andrey <sharginandrei@gmail.com>
2022-11-02 21:24:30 +0300

    123

---
+++ 9/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mashin(true);
+           
+
+
+        }
+        static private  bool mashin(bool f)
+        {
+            int k;
+            k = RecursivKey();
+
+            int first = 0;
+            for (int i = 1; i <= k; i++)
+            {
+                int a;
+
+                a = (int)readNumber(i, k);
+
+                if (a > k)
+                {
+                    if (first == 0) first = a;
+                }
+
+                if (i == k)
+                {
+                    Console.WriteLine("Первое число, большее к = {0}", first);
+                }
+            }
+            Console.WriteLine("Повторить Y|N ?");
+            if (Function(Console.ReadLine()))
+            {
+                return mashin(true);
+            }
+            else return false;
+        }
+        static private int readNumber(int i, int k)
+        {
+            int a;
+            Console.WriteLine("Введите {0} число", i);
+            bool f = int.TryParse(Console.ReadLine(), out a);
+            if (f && a != 0)
+            {
+                return a;
+            }
+            else
+            {
+                Console.WriteLine("Неверное значение");
+                return readNumber(i, k);
+            }
+        }
+        static private int RecursivKey()
+        {
+            int k;
+            Console.WriteLine("Введите целое число К");
+            bool q = int.TryParse(Console.ReadLine(), out k);
+            if (q && k != 0)
+            {
+                return k;
+            }
+            else
+            {
+                Console.WriteLine("Неверные данные");
+                return RecursivKey();
+            }
+        }
+        static private bool Function(string str)
+        {
+            if (str != "Y" || str != "N")
+            {
+
+                return (str == "Y") ? true : false;
+            }
+            else
+            {
+                Console.WriteLine("Неверные данные");
+                return Function(str);
+            }
+
+        }
+    }
+}
---
+++ 9/rec 9.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string qwerty;
+            do
+            {
+                /*
+                 *  k целое чмсло
+                 *  first первое число большее К
+                 *  a числа которые вводятся в цикле
+                 *  i счётчик
+                 *  w поррядковыйф номер числа а
+                */
+                int k;
+                k = RecursivKey();
+                int first = 0;
+                int a, i = 1;
+                int w = 0;
+                do
+                {
+
+                    a = (int)readNumber(i, k);
+
+                    if (a > k)
+                    {
+                        if (first == 0)
+                        {
+                            first = a;
+                            w = i;
+                        }
+                    }
+                    i++;
+                }
+                while (a != 0);
+                if (first > 0) Console.WriteLine("Порядковый номер числа {0} число {1} ", w, first);
+                else Console.WriteLine("Нет чисел больше K");
+                Console.WriteLine("Повторить программу? Y|N");
+            }
+            while (RecursivBool() != "N");
+
+        }
+        /*
+         * читаем строку и проверяем на число
+         */
+        static private int readNumber(int i, int k)
+        {
+            int a;
+            Console.WriteLine("Введите {0} число", i);
+            bool f = int.TryParse(Console.ReadLine(), out a);
+            if (f)
+            {
+                return a;
+            }
+            else
+            {
+                Console.WriteLine("Неверное значение");
+                return readNumber(i, k);
+            }
+        /*
+         * Читаем строку, проверяем на число
+         */
+        }
+        static private int RecursivKey()
+        {
+            int k;
+            Console.WriteLine("Введите целое число К");
+            bool q = int.TryParse(Console.ReadLine(), out k);
+            if (q && k != 0)
+            {
+                return k;
+            }
+            else
+            {
+                Console.WriteLine("Неверные данные");
+                return RecursivKey();
+            }
+        }
+        /*
+         * Читает символ х или у
+         * возращает переменную
+         * иначе повторяется
+         */
+        static private string RecursivBool()
+        {
+            string a = Console.ReadLine();
+
+            if (a == "Y" || a == "N")
+            {
+                return a;
+            }
+            else
+            {
+                Console.WriteLine("Невернные данные, попробуйте ещё!"); 
+                return RecursivBool();
+            }
+        }
+    }
+}
